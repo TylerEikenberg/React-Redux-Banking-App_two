c6eed72664c6c4212ef666da624a70e30fe7537e
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2020-01-05 11:50:58 -0500

    added loan button to homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,7 +3,13 @@ import { useSelector } from "react-redux";
 
 function HomePage() {
   const balance = useSelector(state => state.balance);
-  return <h1>Balance: ${balance}</h1>;
+  return (
+    <div>
+      <h1>Balance: ${balance}</h1>
+      <h2>Loan Required</h2>
+      <button>Apply For Loan</button>
+    </div>
+  );
 }
 
 export default HomePage;
