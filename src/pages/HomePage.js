989bdd0206b1350931afef77f407b74544c71342
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2020-01-05 11:59:16 -0500

    add useDispatch to dispatch loan action on homepage for button

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,13 +1,19 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
 function HomePage() {
   const balance = useSelector(state => state.balanceReducer.balance);
+  const loan = useSelector(state => state.loanReducer.loan);
+  const dispatch = useDispatch();
+
+  function loanApplyHandle() {
+    dispatch({ type: "APPLY" });
+  }
   return (
     <div>
       <h1>Balance: ${balance}</h1>
-      <h2>Loan Required</h2>
-      <button>Apply For Loan</button>
+      <h2>{loan ? "Loan Approved" : "Loan Required"}</h2>
+      <button onClick={loanApplyHandle}>Apply For Loan</button>
     </div>
   );
 }
