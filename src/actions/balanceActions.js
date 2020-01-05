b0442e5c3990b91a701cb9f933f7afcc4996ec5b
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2020-01-05 12:14:37 -0500

    added loading function to actions, dispatch loading during depositAsync function

--- src/actions/balanceActions.js
+++ src/actions/balanceActions.js
@@ -1,9 +1,14 @@
+export function loading() {
+  return { type: loading };
+}
+
 export function deposit() {
   return { type: "DEPOSIT", payload: 10 };
 }
 
 export function depositAsync() {
   return dispatch => {
+    dispatch(loading());
     setTimeout(() => {
       dispatch(deposit());
     }, 1000);
