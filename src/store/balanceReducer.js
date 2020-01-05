e8ae26b60a1def11f50351f5b64c9ed60608954b
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2020-01-05 12:12:45 -0500

    added loading state to balanceReducer to handle async waiting

--- src/store/balanceReducer.js
+++ src/store/balanceReducer.js
@@ -1,13 +1,16 @@
 const initState = {
-  balance: 0
+  balance: 0,
+  loading: false
 };
 
 export default function balanceReducer(state = initState, action) {
   switch (action.type) {
     case "DEPOSIT":
-      return { balance: state.balance + action.payload };
+      return { balance: state.balance + action.payload, loading: false };
     case "WITHDRAW":
-      return { balance: state.balance - action.payload };
+      return { balance: state.balance - action.payload, loading: false };
+    case "LOADING":
+      return { ...state, loading: true };
     default:
       return state;
   }
