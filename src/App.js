a78523986ab7eebb53f9283d652b46c844fe5dc5
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2020-01-05 11:34:03 -0500

    created pages and routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Route, NavLink } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <ul style={{ display: "flex" }}>
+            <li className="li-style">
+              <NavLink className="App-link" to="/">
+                Home
+              </NavLink>
+            </li>
+            <li className="li-style">
+              <NavLink className="App-link" to="/deposit">
+                Deposit
+              </NavLink>
+            </li>
+            <li className="li-style">
+              <NavLink className="App-link" to="/withdraw">
+                Withdraw
+              </NavLink>
+            </li>
+          </ul>
+        </header>
+      </div>
+      <Route path="/" />
+      <Route path="/deposit" />
+      <Route path="/withdraw" />
+    </BrowserRouter>
   );
 }
 
