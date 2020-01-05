11ba1c9331cea8ff482f2753c5d9078d9214df1d
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2020-01-05 16:12:54 -0500

    adding login form, styling

--- src/components/loginForm/LoginForm.js
+++ src/components/loginForm/LoginForm.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 function LoginForm() {
-  return <div>hello</div>;
+  return (
+    <div>
+      <form className="LoginForm-form">
+        <label>Login</label>
+        <input type="text" placeholder="username" />
+      </form>
+    </div>
+  );
 }
 
 export default LoginForm;
